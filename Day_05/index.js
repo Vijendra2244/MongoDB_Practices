2d6137779fdb929b5a96faeae1471c099c3fde87
vijendra2244 <vs230267@gmail.com>
2024-02-25 23:33:32 +0530

    socket.io

--- Day_05/index.js
+++ Day_05/index.js
@@ -1,22 +1,45 @@
-const { WebSocketServer } = require("ws");
+// // const { WebSocketServer } = require("ws");
 
-const wss = new WebSocketServer({ port: 8080 });
+// // const wss = new WebSocketServer({ port: 8080 });
 
-//Syntex
-wss.on("connection", (socket) => {
-  // console.log("A new connection was made")
-  socket.on("message", (msg) => {
-    console.log("Client " + msg)
-    if (msg.toString() === "Hey") {
-      socket.send("Hello");
-    } else if (msg.toString() === "bye") {
-      socket.send("ta ta");
-    } else {
-      socket.send("see you");
-    }
-  });
-});
+// // //Syntex
+// // wss.on("connection", (socket) => {
+// //   // console.log("A new connection was made")
+// //   socket.on("message", (msg) => {
+// //     console.log("Client " + msg)
+// //     if (msg.toString() === "Hey") {
+// //       socket.send("Hello");
+// //     } else if (msg.toString() === "bye") {
+// //       socket.send("ta ta");
+// //     } else {
+// //       socket.send("see you");
+// //     }
+// //   });
+// // });
 
-//   socket.on("close",()=>{
-//     console.log("Client disconnected")
-//   }) 
+// // //   socket.on("close",()=>{
+// // //     console.log("Client disconnected")
+// // //   }) 
+
+
+//  const {EventEmitter}  = require("events")
+
+
+//  const Button = new EventEmitter()
+
+
+// //event handling
+// Button.on("click",()=>{
+//   console.log("Button is clicked")
+// })
+
+// Button.on("xyz",()=>{
+//   console.log("something is happened")
+// })
+
+
+
+
+// //event triggered
+// Button.emit("click")
+// Button.emit("xyz")
---
+++ Day_06/index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+// console.log(app)
+
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+
+server.listen(8080, () => {
+  console.log("server is listen 8080");
+});
+
+const webSocketServer = new Server(server);
+
+webSocketServer.on("connection", (socket) => {
+  socket.emit("xyz", "hello");
+});
+
+
+
+// socket.on("xyz", (msg) => {
+//   console.log(msg);
+// });this is was an example becuse we wil done this thing on frntend
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/",(req,res)=>{
+//     res.send("home page")
+// })
+
+// app.listen(8080,()=>{
+//     console.log(`server is listening on port 8080`)
+// })
+
+// const { EventEmitter } = require("events");
+
+// const player = new EventEmitter();
+
+// player.on("shot", (number) => {
+//   console.log(`Player number ${number} got injured`);
+// });
+
+// player.emit("shot", 1);
+
+// //    player.emit("disqualified",3)
+
+// // player.on("dead",(number)=>{
+// //     console.log(`Player number ${number} is dead`)
+// // })
+
+// // player.on("disqualified",(number)=>{
+// //     console.log(`Player number ${number} is disqualified`)
+// // })
+// // player.emit("dead", 2);
